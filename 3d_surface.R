4898b60b62a8ab623d7ab1453be8f8e5bad3dd01
cesarium <antoniodnc99@protonmail.com>
2021-05-11 00:07:20 +0100

    3D surface plot & MCMC file

---
+++ 3d_surface.R
@@ -0,0 +1,82 @@
+library('ggplot2')
+library('rgl')
+
+m_estimate = 1.773521e+68
+
+mcmc <- read.csv(file = 'mcmc.R',sep=',', header=FALSE)
+names(mcmc) = c('nu.chain','g.chain')
+
+# #########################################################################################
+# Stuff already in Jupyter Notebook
+# #########################################################################################
+
+dados <- read.csv(file = 'data_Antonio.R',sep=' ', header=FALSE)
+names(dados) = c('freq','intensity')
+
+Nu.data <- dados[1]
+G.data <- dados[2]
+
+sigma <- 0.05
+N <- 101
+
+nu.min <- Nu.data$freq[1]
+nu.max <- tail(Nu.data$freq,n=1)
+Delta.nu <- nu.max - nu.min
+
+dnu = Nu.data$freq[2]-Nu.data$freq[1]
+g.min = dnu/2
+g.max = sqrt(log(2)/pi)/sigma
+Delta.g = g.max - g.min
+
+nu_i = Nu.data$freq
+d_i = G.data$intensity
+S_1 = sum((d_i-1)^2)
+
+# #############################################################################
+
+f_i <- function(nu,g){
+  
+  exp(-log(2)*(nu_i-nu)^2/g^2) * sqrt(log(2)/pi) / g
+  
+}
+
+# #############################################################################
+
+log.Likelihood <- function(nu,g){
+  fi = f_i(nu,g)
+  
+  (-0.5/sigma^2) * ( S_1 + sum( fi^2 - 2*(d_i-1)*fi ) )
+}
+
+unnormalized.posterior <- function(nu,g){
+  
+  prior = 1 / ( Delta.nu * log(g.max/g.min) * g)
+  L.likelihood = log.Likelihood(nu,g) 
+  
+  prior * exp(L.likelihood) / (sigma * sqrt(2*pi))^N
+}
+
+vec_unnormalized.posterior <- function(Nu,G){Vectorize(unnormalized.posterior,vectorize.args = c('nu','g'))(Nu,G)}
+
+Gp.posterior <- function(nu,g){ vec_unnormalized.posterior(nu,g) / m_estimate } 
+
+# #########################################################################################
+# #########################################################################################
+
+# #########################################################################################
+# 3D plotting
+# #########################################################################################
+x = seq(1417,1419,length.out = 100)
+y = seq(1.4,2.8,length.out = 100)
+z = outer(x,y,Gp.posterior)
+
+open3d()
+
+persp3d(x = x,
+        y = y,
+        z = z,
+        col = gray.colors(100, start =1, end = 0),
+        xlab = "v0 (MHz)",
+        ylab = "g",
+        zlab = "p(v0,g|D,M,I)"
+)
